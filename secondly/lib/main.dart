42e64ebee10581dbf4b03d0ba3396f783544080d
obay-it <info.obaymichael@gmail.com>
2024-01-17 22:34:10 +0100

    vfinal

--- secondly/lib/main.dart
+++ secondly/lib/main.dart
@@ -1,20 +1,58 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(const MyWidget());
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Center(
-            child: Text("Obay.com"),
+          appBar: AppBar(
+            title: const Center(
+              child: Text(
+                "Obay.com",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            backgroundColor: const Color.fromARGB(255, 0, 0, 0),
           ),
-          backgroundColor: Colors.deepPurple[400],
-        ),
-        body: const Center(
-          child: Text("learn to code with Dmc"),
-        ),
-        backgroundColor: Colors.deepPurple[200],
-      ),
-    ),
-  );
+          body: SafeArea(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.sp,
+            children: [
+              Container(
+                color: const Color.fromARGB(255, 33, 243, 243),
+                width: 150.0,
+                height: 150.0,
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(20.0),
+                margin: EdgeInsets.all(0.0),
+                child: const Text('Colum One'),
+              ),
+              Container(
+                color: Colors.blue,
+                width: 150.0,
+                height: 150.0,
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(20.0),
+                margin: EdgeInsets.all(0.0),
+                child: const Text('Colum two'),
+              ),
+              Container(
+                color: const Color.fromARGB(255, 79, 33, 243),
+                width: 150.0,
+                height: 150.0,
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(20.0),
+                margin: EdgeInsets.all(0.0),
+                child: const Text('Colum three'),
+              ),
+            ],
+          ))),
+    );
+  }
 }
