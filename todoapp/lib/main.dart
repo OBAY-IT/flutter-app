084b9882ed463b70c925a5e0301701ab7607e3ef
obay-it <info.obaymichael@gmail.com>
2024-01-22 00:27:30 +0100

    v2

--- todoapp/lib/main.dart
+++ todoapp/lib/main.dart
@@ -1,69 +1,53 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(FloatingButtonDemo());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class FloatingButtonDemo extends StatefulWidget {
+  const FloatingButtonDemo({super.key});
 
+  @override
+  State<FloatingButtonDemo> createState() => _FloatingButtonDemoState();
+}
+
+class _FloatingButtonDemoState extends State<FloatingButtonDemo> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+        home: Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text('ToDo List'),
+        centerTitle: true,
+        backgroundColor: const Color.fromRGBO(82, 170, 94, 1.0),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+      body: Container(
+        padding: EdgeInsets.all(32),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        backgroundColor: const Color.fromRGBO(82, 170, 94, 1.0),
+        onPressed: () {
+          openDialog();
+        },
+        label: const Text('Task'),
+        icon: const Icon(Icons.add, color: Colors.white, size: 25),
+      ),
+    ));
+  }
+
+  Future openDialog() => showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text("New Task"),
+          content: TextField(
+            decoration: InputDecoration(hintText: 'Enter your Task'),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {},
+              child: Text('SUBMIT'),
+            )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
+      );
 }
